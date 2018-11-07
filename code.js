68581e8d94b1555197713804b896ec6d39ea93eb
li008772 <43221713+li008772@users.noreply.github.com>
2018-11-07 00:56:14 -0600

    file1

--- code.js
+++ code.js
@@ -25,8 +25,9 @@ function updatelocation()
 	var textInput = document.getElementById("name").value;
 	var findplace = textInput.replace(/ /g,"%20");
 	//const request = new XMLHttpRequest();
-	var address = "https://us1.unwiredlabs.com/v2/address.php?type="+type+"&token="+token+"&formate=json&q="+findplace;
+	var address = "https://us1.locationiq.com/v1/search.php?key="+token+"&q="+findplace+"&format=json";
     const request1 = new XMLHttpRequest();
+  
   if ("withCredentials" in request1) {
 
     request1.open("GET", address, true);
@@ -42,11 +43,22 @@ function updatelocation()
 	{
 		if(this.readyState==4&&this.status==200)
 		{
-			alert(request.responseText);
+			
+			var theanswer = JSON.parse(request1.responseText);
+	        mymap.panTo(new L.LatLng(theanswer[0].lat, theanswer[0].lon));
+			var location1 = document.getElementById("latitude").value;
+	        var location2 = document.getElementById("longtitude").value;
+			if(location1 != null && location2 != null)
+	        {
+	         var center = mymap.getCenter();
+	         location1 = center.lat;
+	         location2 = center.lng;
+	        }
+			
 		}
 		else if(this.status>=400)
 		{
-			alert("error");
+			alert("invalid input, try again");
 		}
 	}
   
@@ -81,6 +93,50 @@ function updateBox()
 	location1.value = center.lat;
 	location2.value = center.lng;
 	}
+	var location3 = document.getElementById("goto");
+	var token = "6352d14a272592";
+	var lat = center.lat;
+	var lon = center.lng;
+	//const request = new XMLHttpRequest();
+	var address = "https://us1.locationiq.com/v1/reverse.php?key="+token+"&lat="+lat+"&lon="+lon+"&format=json";
+	console.log(address);
+	const request1 = new XMLHttpRequest();
+  
+  if ("withCredentials" in request1) {
+
+    request1.open("GET", address, true);
+
+  } 
+  else
+	  {
+
+    xhr = null;
+
+  }
+  request1.onreadystatechange=function()
+	{
+		if(this.readyState==4&&this.status==200)
+		{
+		
+			var theanswer = JSON.parse(request1.responseText);
+			console.log(theanswer);
+			var thename = document.getElementById("name");
+			console.log(thename.value);
+			if(thename.value != null)
+	        {
+	           thename.value = theanswer.display_name.split(',')[0];
+			   console.log(theanswer.display_name.indexOf(","));
+	        }
+			
+		}
+		else
+		{
+			var thename = document.getElementById("name").value;
+			thename = "no match";
+		}
+
+	}
+	request1.send();
 }
 
 function test()
@@ -95,8 +151,107 @@ function fullscreen()
 	{
 	elem1.webkitRequestFullscreen();
 	}
+}
 
+
+
+function updateTable(){
+	var center = mymap.getCenter();
+	var lat1 = center.lat;
+	var long1 = center.lng;
+	var count = 0;
+	var store = [100];
+	var i=0;
+	var chemical =["co","o3","no2","so2","pm10","pm25"];
+	var chemical =["pm25","pm10","so2","no2","o3","co"];
+	var hit = 0;
+	var row;
+	var cell;
+	var trigger = false;
 	
+	var edgeNorth = mymap.getBounds().getNorth();
+	var edgeEast = mymap.getBounds().getEast();
+	var edgeSouth = mymap.getBounds().getSouth();
+	var edgeWest = mymap.getBounds().getWest();
 	
+	var edgeNW = mymap.getBounds().getNorthWest();
+	var lat2 = edgeNW.lat;
+	var long2 = edgeNW.lng;
+	
+	//https://api.openaq.org/v1/measurements
+	//https://api.openaq.org/v1/locations
+	
+	var R = haversine(lat1, long1, lat2, long2);
+	//console.log(haversine(lat1, long1, lat2, long2));
+	var table = document.getElementById("mytable");
+
+	$.ajax({
+		url: "https://api.openaq.org/v1/latest?coordinates="+lat1+","+long1+"&radius="+R,
+		dataType: 'json',
+		type: 'get',
+		cache: false,
+		success: function(data){
+
+			$(data.results).each(function(index, value){
+				store[count] = value.measurements;
+				row = table.insertRow(count);
+				console.log(value);
+				console.log(store[count]);
+				for(x=0;x<chemical.length;x++)
+				{
+				for(i=0;i<store[count].length;i++)
+				{
+
+						console.log(x+" this is the length");
+						console.log(chemical[x] + " " + store[count][i].parameter);
+						if(store[count][i].parameter== chemical[x])
+					{
+					cell = row.insertCell(x);
+			        cell.innerHTML = store[count][i].value;
+					trigger = true;
+					break;
+
+					}
+				    }
+					
+					    if(trigger == false)
+						{
+						cell = row.insertCell(x);
+			            cell.innerHTML = "null";
+						}
+						trigger = false;
+					
+				}
+				
+				count++;
+
+
+				
+			});
+			
+		}
+	});
+	/*
+	for(i=0;i<store.length;i++)
+	{
+		console.log("row update" + store[0]);
+		row = table.insertRow(i);
+		for(l=0;l<store[i].length;l++)
+		{
+			cell = row.insertCell(l);
+			cell.innerHTML = store[i][l].value;
+		}
+	}*/
+}
+
+function haversine(){ //haversine formula code borrowed from Rosetta Code https://rosettacode.org/wiki/Haversine_formula#Java
+	var radians = Array.prototype.map.call(arguments, function(deg) { return deg/180.0 * Math.PI; });
+    var lat1 = radians[0], lon1 = radians[1], lat2 = radians[2], lon2 = radians[3];
+    var R = 6371e3; // meters
+    var dLat = lat2 - lat1;
+    var dLon = lon2 - lon1;
+    var a = Math.sin(dLat / 2) * Math.sin(dLat /2) + Math.sin(dLon / 2) * Math.sin(dLon /2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.asin(Math.sqrt(a));
+    return R * c;
 }
 
